ae0cd9df9955472895a603c825d57aae76753f19
Michael O'Brien <michael@obrienlabs.org>
2022-05-29 21:48:53 -0400

    #8 - add JPA Entities

---
+++ reference-sbi/src/main/java/ca/cloudlift/reference/sbi/model/jpa/Event.java
@@ -0,0 +1,99 @@
+package ca.cloudlift.reference.sbi.model.jpa;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class Event {
+
+  @Id
+  @Column(name="ID")
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  
+  private Long timestamp;
+  
+  private String state;
+  
+  @ElementCollection
+  private Map<String, String> map = new HashMap<>();
+  
+  @ManyToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+  @JoinTable(name="EVENT_SEL",
+		  joinColumns=@JoinColumn(name="EVENT_ID", referencedColumnName="ID"),
+		  inverseJoinColumns=@JoinColumn(name="SELECT_ID", referencedColumnName="ID"))
+  private Set<Selector> selectors = new HashSet<>();
+  
+
+  protected Event() {}
+
+  public Event(Long timestamp, String state) {
+	  this.timestamp = timestamp;
+	  this.state = state;
+
+  }
+  
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Map<String, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+
+	public Set<Selector> getSelectors() {
+		return selectors;
+	}
+
+	public void setSelectors(Set<Selector> selectors) {
+		this.selectors = selectors;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", timestamp=" + timestamp + ", state=" + state + ", map=" + map + ", selectors="
+				+ selectors + "]";
+	}
+
+
+
+}
---
+++ reference-sbi/src/main/java/ca/cloudlift/reference/sbi/model/jpa/Selector.java
@@ -0,0 +1,103 @@
+package ca.cloudlift.reference.sbi.model.jpa;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Selector {
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String tag;
+
+	@ManyToMany(mappedBy = "selectors")
+	private List<Event> events;
+
+	public Selector() {
+	}
+
+	public Selector(String tag) {
+		super();
+		this.tag = tag;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+	
+	public void removeEvent(Event event) {
+		this.events.remove(event);
+	}
+	
+	public void addEvent(Event event) {
+		this.events.add(event);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((tag == null) ? 0 : tag.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Selector other = (Selector) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (tag == null) {
+			if (other.tag != null)
+				return false;
+		} else if (!tag.equals(other.tag))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Selector [id=" + id + ", tag=" + tag + "]";
+	}
+
+}
